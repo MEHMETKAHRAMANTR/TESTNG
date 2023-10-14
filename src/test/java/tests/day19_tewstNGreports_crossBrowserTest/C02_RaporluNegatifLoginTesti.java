cc4c0919ee0f7f16f3e366fff75af9419a2513cb
MEHMETKAHRMANTR <mehmetkahraman.2028@gmail.com>
2023-10-14 18:32:06 +0300

    14.10.2023

---
+++ src/test/java/tests/day19_tewstNGreports_crossBrowserTest/C02_RaporluNegatifLoginTesti.java
@@ -0,0 +1,45 @@
+package tests.day19_tewstNGreports_crossBrowserTest;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.QualitydemyPage;
+import utilities.Driver;
+import utilities.ReusableMethods;
+public class C02_RaporluNegatifLoginTesti {
+    QualitydemyPage qualitydemyPage;
+    @Test
+    public void gecersizPasswordTest(){
+        Driver.getDriver().get("https://www.qualitydemy.com/");
+        qualitydemyPage= new QualitydemyPage();
+        qualitydemyPage.ilkLoginLinki.click();
+        qualitydemyPage.emailKutusu.sendKeys("anevzatcelik@gmail.com");
+        qualitydemyPage.passwordKutusu.sendKeys("12345");
+        qualitydemyPage.loginButonu.click();
+        ReusableMethods.bekle(1);
+        Assert.assertTrue(qualitydemyPage.emailKutusu.isDisplayed());
+        Driver.closeDriver();
+    }
+    @Test
+    public void gecersizEmailTesti(){
+        Driver.getDriver().get("https://www.qualitydemy.com/");
+        qualitydemyPage= new QualitydemyPage();
+        qualitydemyPage.ilkLoginLinki.click();
+        qualitydemyPage.emailKutusu.sendKeys("alican@gmail.com");
+        qualitydemyPage.passwordKutusu.sendKeys("Nevzat152032");
+        qualitydemyPage.loginButonu.click();
+        ReusableMethods.bekle(1);
+        Assert.assertTrue(qualitydemyPage.emailKutusu.isDisplayed());
+        Driver.closeDriver();
+    }
+    @Test
+    public void gecersizEmailPasswordTesti(){
+        Driver.getDriver().get("https://www.qualitydemy.com/ ");
+        qualitydemyPage= new QualitydemyPage();
+        qualitydemyPage.ilkLoginLinki.click();
+        qualitydemyPage.emailKutusu.sendKeys("alican@gmail.com");
+        qualitydemyPage.passwordKutusu.sendKeys("12345");
+        qualitydemyPage.loginButonu.click();
+        ReusableMethods.bekle(1);
+        Assert.assertTrue(qualitydemyPage.emailKutusu.isDisplayed());
+        Driver.closeDriver();
+    }
+}
