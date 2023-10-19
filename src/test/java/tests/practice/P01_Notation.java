48f01693a5b23a0a3fa402573a836b8c77e090e4
MEHMETKAHRMANTR <mehmetkahraman.2028@gmail.com>
2023-10-19 21:06:31 +0300

    tetsNG

---
+++ src/test/java/tests/practice/P01_Notation.java
@@ -0,0 +1,26 @@
+package tests.practice;
+
+import org.testng.annotations.Test;
+
+public class P01_Notation {
+
+    @Test
+    public void Nergiz() {
+        System.out.println("Nergiz");
+    }
+
+    @Test
+    public void Omer() {
+        System.out.println("Omer");
+    }
+
+    @Test
+    public void Senol() {
+        System.out.println("Senol");
+    }
+
+    @Test
+    public void Bugra() {
+        System.out.println("Bugra");
+    }
+}
